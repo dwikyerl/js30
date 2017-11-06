e16bf910e886e7b896d363c61f817a452bd41394
Dwiky Erlangga <dwikyerl@gmail.com>
2017-11-06 11:15:47 +0700

    add clock for project 2

---
+++ 02 clock/main.js
@@ -0,0 +1,22 @@
+const secondHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate() {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    secondHand.style.transition = (seconds === 0) ?  'all 0s' : 'all 0.05s';
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + 90;
+    minHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+    const hours = now.getHours();
+    const hoursDegrees = ((hours / 12) * 360) + 90;
+    console.log(hoursDegrees);
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
